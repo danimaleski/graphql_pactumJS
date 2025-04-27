4f4da969cac98b6fc658adc96dd36ef5464c30ec
Daniella Maleski <daniella.maleski@gmail.com>
2025-04-27 15:53:15 -0300

    graphql aula

---
+++ test/graphql/user.test.js
@@ -0,0 +1,54 @@
+// test.js
+const { spec } = require('pactum');
+const { eachLike, like, custom } = require('pactum-matchers');
+
+//let token;
+beforeEach(async () => {
+    await spec()
+        .post('http://lojaebac.ebaconline.art.br/graphql')
+        .withGraphQLQuery(`
+        mutation AuthUser($email: String, $password: String) {
+            authUser(email: $email, password: $password) {
+              success
+              token
+            }
+          }
+    `)
+        .withGraphQLVariables({
+            "email": "admin@admin.com",
+            "password": "admin123"
+        })
+        .stores('token', 'data.authUser.token')
+
+})
+
+it('listagem de usuarios', async () => {
+    await spec()
+        .post('http://lojaebac.ebaconline.art.br/graphql')
+        .withHeaders("Authorization", `#token`)
+        .withGraphQLQuery(`
+        query {
+            Users {
+              id
+              email
+              profile {
+                firstName
+              }
+            }
+          }
+    `)
+        .expectStatus(200)
+        .inspect()
+        .expectJsonMatch({
+            data: {
+                Users: eachLike({
+                    id: like("657b05fe31b986f1c0a7a053"),
+                    email: like("cliente@ebac.art.br"),
+                    profile: {
+                        //firstName: like("Cliente")
+                        firstName: custom(val => typeof val === 'string' || val === null)
+                    }
+                })
+            }
+        })
+});
